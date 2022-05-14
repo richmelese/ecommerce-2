f53ee3291e36836b2acc56b676ae6c84a1968fb8
Chalew Tesfaye <chalew.t29@gmail.com>
2022-05-14 18:50:06 +0300

    minor typo error

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,65 +1,63 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\product;
 use Illuminate\Http\Request;
-rar
+
 class ProductController extends Controller
 {
-    //
-    public function register()
+  //
+  public function register()
   {
-  return view('product.register');
+    return view('product.register');
   }
-    function store(Request $request)
-    
-    {
-      $product =new product();
-      $product->name = $request->name;
-      $product->unit = $request->unit;
-      $product->price = $request->price;
-      $product->quantity = $request->quantity;
-     $is_saved = $product->save();
-    if($is_saved){
-    echo " MR DEMEKE YIDEG YOUR DATA SAVED SUCCESSFULLY.";
-               }
-    else{
-     echo "Sorry, try again something went wrong.";
-       }
+  function store(Request $request)
 
-       
+  {
+    $product = new product();
+    $product->name = $request->name;
+    $product->unit = $request->unit;
+    $product->price = $request->price;
+    $product->quantity = $request->quantity;
+    $is_saved = $product->save();
+    if ($is_saved) {
+      echo " MR DEMEKE YIDEG YOUR DATA SAVED SUCCESSFULLY.";
+    } else {
+      echo "Sorry, try again something went wrong.";
     }
-    public function get_all()
-{
- $product = product::all();
- return view('product.list', compact('product'));
-}
-public function edit($id)
- {
- $product = product::find($id);
- return view('product.edit', compact('product'));
- }
- public function update(Request $request)
- {
- //Validate
- $request->validate(['name' => 'required' ]);
-  $product = product::find($request->id);
-  $product->name = $request->name;
-  $product->unit = $request->unit;
-  $product->price = $request->price;
-  $product->quantity = $request->quantity;
-  $product->save();
-  return redirect('product/list');
+  }
+  public function get_all()
+  {
+    $product = product::all();
+    return view('product.list', compact('product'));
+  }
+  public function edit($id)
+  {
+    $product = product::find($id);
+    return view('product.edit', compact('product'));
+  }
+  public function update(Request $request)
+  {
+    //Validate
+    $request->validate(['name' => 'required']);
+    $product = product::find($request->id);
+    $product->name = $request->name;
+    $product->unit = $request->unit;
+    $product->price = $request->price;
+    $product->quantity = $request->quantity;
+    $product->save();
+    return redirect('product/list');
   }
   public function delete($id)
- {
- product::where('id', $id)->delete();
- return redirect('product/list');
- }
+  {
+    product::where('id', $id)->delete();
+    return redirect('product/list');
+  }
 
- public function get_by_id($id)
- {
-  $product = product::where('id', $id)->first();
-  return view('product.search', compact('product'));
+  public function get_by_id($id)
+  {
+    $product = product::where('id', $id)->first();
+    return view('product.search', compact('product'));
   }
 }
--- app/Models/Catagory.php
+++ app/Models/Catagory.php
@@ -9,8 +9,5 @@ class Catagory extends Model
 {
     use HasFactory;
     protected $fillable = [ 'name'];
-<<<<<<< HEAD
-=======
 }
->>>>>>> fbd88377e3db5f5c54e7a5b2199b30b33da2c1cd
-}
+
